ede3dba1d72fa2078b5a4e129fcedff1a04e993a
Aaperi <apattman@gmail.com>
2015-11-23 11:14:55 +0100

    enemyShooting
    Added enemyShooting script

---
+++ Technical/Vertical Slice/INDIE/Assets/Scripts/enemyShooting.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyShooting : MonoBehaviour {
+
+    public GameObject bullet;
+
+    private SphereCollider col;
+    private Transform player;
+    private bool shooting;
+    
+
+    void Awake()
+    {
+        col = GetComponent<SphereCollider>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    void Update()
+    {
+        if (!shooting)
+            
+            Shoot();
+
+    }
+
+
+    void Shoot()
+    {
+        shooting = true;
+
+        float fractionalDistance = (col.radius - Vector3.Distance(transform.position, player.position)) / col.radius;
+
+        GameObject _bullet = Instantiate(bullet, transform.position + transform.forward, transform.rotation) as GameObject;
+        _bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 500f);
+
+    }
+
+
+    void ShotEffects()
+    {
+        
+    }
+}
