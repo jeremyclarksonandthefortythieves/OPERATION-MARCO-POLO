aab0b627d0bfe5148522cf37af8c9595396c4b49
Aaperi <apattman@gmail.com>
2015-11-30 16:24:43 +0100

    HUDscript
    Crouching and Standing image changing

--- Technical/Vertical Slice/INDIE/Assets/Scripts/HUDscript.cs
+++ Technical/Vertical Slice/INDIE/Assets/Scripts/HUDscript.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class HUDscript : MonoBehaviour {
 
+    public Image crouch;
+    public Image stand;
+
+    private PlayerControl player;
+    
 	// Use this for initialization
 	void Start () {
-	
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
+        crouch.enabled = false;
+        stand.enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (player.sneaking)
+        {
+            crouch.enabled = true;
+            stand.enabled = false;
+        }
+            
+        else
+        {
+            stand.enabled = true;
+            crouch.enabled = false;
+        }
 	}
 }
--- Technical/Vertical Slice/INDIE/Assets/Scripts/enemyAI.cs
+++ Technical/Vertical Slice/INDIE/Assets/Scripts/enemyAI.cs
@@ -79,4 +79,5 @@ public class enemyAI : MonoBehaviour {
 			ChaseTimer = 0;
 
 	}
+
 }
