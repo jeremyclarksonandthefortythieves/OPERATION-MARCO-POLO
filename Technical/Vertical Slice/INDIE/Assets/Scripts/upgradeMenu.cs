5875a2cb690505ef0cedc13941b4d9e91dff83d3
Frits Hensen <fritsjehensen@gmail.com>
2015-12-02 20:10:48 +0100

    Changes

--- Technical/Vertical Slice/INDIE/Assets/Scripts/upgradeMenu.cs
+++ Technical/Vertical Slice/INDIE/Assets/Scripts/upgradeMenu.cs
@@ -2,97 +2,84 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class upgradeMenu : MonoBehaviour {
+public class upgradeMenu : MonoBehaviour
+{
 
-    public Canvas upgradeScreen;
-    public Image silencer,
-                 Smoke,
-                 Mine;
-    public Text notEnough,
-                tokens;
+	public Canvas upgradeScreen;
+	public Image silencer,
+				 Smoke,
+				 Mine;
+	public Text notEnough,
+				tokens;
 
-    private PlayerControl player;
-    private float textTimer = 2;
+	//private PlayerControl player;
+	private LoadSaveScript player;
+	private float textTimer = 2;
 
 	// Use this for initialization
-	void Start () {
-        player = GameObject.Find("menuController").GetComponent<PlayerControl>();
-        notEnough.enabled = false;
+	void Start() {
+		//  player = GameObject.Find("menuController").GetComponent<PlayerControl>();
+		notEnough.enabled = false;
+		player = GameObject.FindGameObjectWithTag("GameController").GetComponent<LoadSaveScript>();
+		player.Load();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-        tokens.text = player.money.ToString();
+	void Update() {
+		tokens.text = player.saveData.money.ToString();
 
-        if (notEnough.enabled)
-        {
-            textTimer -= Time.deltaTime;
+		if (notEnough.enabled) {
+			textTimer -= Time.deltaTime;
 
-            if(textTimer <= 0)
-            {
-                notEnough.enabled = false;
-                textTimer = 2;
-            }
-        }
+			if (textTimer <= 0) {
+				notEnough.enabled = false;
+				textTimer = 2;
+			}
+		}
 	}
 
-    public void Silencer()
-    {
-        if(player.money >= 3)
-        {
-            player.money -= 3;
-			player.silencerEnabled = true;
-            silencer.enabled = false;
-        }
+	public void Silencer() {
+		if (player.saveData.money >= 3) {
+			player.saveData.money -= 3;
+			player.saveData.silencer = true;
+			silencer.enabled = false;
+		} else if (player.saveData.money < 3) {
+			notEnough.enabled = true;
+		}
 
-        else if(player.money < 3)
-        {
-            notEnough.enabled = true;
-        }
+	}
 
-    }
-
-    public void smokeGrenade()
-    {
-        if (player.money >= 3)
-        {
-            player.money -= 3;
-			player.smokeAmount += 1;
+	public void smokeGrenade() {
+		if (player.saveData.money >= 3) {
+			player.saveData.money -= 3;
+			player.saveData.smoke += 1;
 			Smoke.enabled = false;
-        }
+		} else if (player.saveData.money < 3) {
+			notEnough.enabled = true;
+		}
+	}
 
-        else if (player.money < 3)
-        {
-            notEnough.enabled = true;
-        }
-    }
-
-    public void proximityMine()
-    {
-        if (player.money >= 3)
-        {
-            player.money -= 3;
-			player.distractionAmount += 1;
+	public void proximityMine() {
+		if (player.saveData.money >= 3) {
+			player.saveData.money -= 3;
+			player.saveData.mine += 1;
 			Mine.enabled = false;
-        }
+		} else if (player.saveData.money < 3) {
+			notEnough.enabled = true;
+		}
+	}
 
-        else if (player.money < 3)
-        {
-            notEnough.enabled = true;
-        }
-    }
+	public void weaponDMGU1() {
+		if (player.saveData.money >= 2) {
+			player.saveData.money -= 2;
+			// player.saveData.bulletDamage += 2;
+		} else if (player.saveData.money < 2) {
+			notEnough.enabled = true;
+		}
+	}
 
-    public void weaponDMGU1()
-    {
-        if(player.money >= 2)
-        {
-            player.money -= 2;
-            player.bulletDamage += 2;
-        }
-
-        else if(player.money < 2)
-        {
-            notEnough.enabled = true;
-        }
-    }
+	public void NextLevel() {
+		player.SaveInUI();
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
 }
