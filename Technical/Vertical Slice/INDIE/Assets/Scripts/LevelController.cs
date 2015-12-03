c20b36eba0e23bcde6b763b1eb374cac074921d6
Frits Hensen <fritsjehensen@gmail.com>
2015-12-03 20:26:07 +0100

    Animations and fade in/out @start and EndLevel

--- Technical/Vertical Slice/INDIE/Assets/Scripts/LevelController.cs
+++ Technical/Vertical Slice/INDIE/Assets/Scripts/LevelController.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LevelController : MonoBehaviour {
 
 	public bool noDetection = true;
 	public bool noKills = true;
-	public GameObject loadUI;
+	public GameObject fadePanel;
 
 	private GameObject gameController;
 	private GameObject player;
@@ -14,6 +15,8 @@ public class LevelController : MonoBehaviour {
 		player = GameObject.FindGameObjectWithTag("Player");
 		gameController = GameObject.FindGameObjectWithTag("GameController");
 		gameController.GetComponent<LoadSaveScript>().Load();
+		StartCoroutine(FadeIn());
+
 	}
 
 	//Checks if objectives are complete and gives player exp
@@ -28,8 +31,43 @@ public class LevelController : MonoBehaviour {
 			player.GetComponent<PlayerControl>().GetExp();
 			Debug.Log("completed without kills!");
 		}
-		NextLevel();
+		//GameObject fade = Instantiate(fadePanel) as GameObject;
+		//fade
+		StartCoroutine(FadeOut());
+		//NextLevel();
 		//spawns the upgrade menu here with button for next level
+	}
+
+	IEnumerator FadeOut() {
+		float i = 0;
+		Image img = fadePanel.GetComponent<Image>();
+		Color col = img.color;
+
+        while (i < 1.2f) {
+			i += 1f * Time.deltaTime;
+
+			col.a = i;
+			img.color = col;
+
+			yield return null;
+        }
+		NextLevel();
+	}
+
+	IEnumerator FadeIn() {
+		float i = 1.1f;
+		Image img = fadePanel.GetComponent<Image>();
+		Color col = img.color;
+
+
+		while (i >= 0f) {
+			i -= 1f * Time.deltaTime;
+
+			col.a = i;
+			img.color = col;
+
+			yield return null;
+		}
 	}
 
 	//asyncs load new level in the background
